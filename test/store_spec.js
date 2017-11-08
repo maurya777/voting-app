9e9a9286d294e7cca3ad20cf81fce91b48ecb789
Satish Maurya <maurya777@gmail.com>
2017-11-08 22:04:48 +0530

    Added redux

---
+++ test/store_spec.js
@@ -0,0 +1,21 @@
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: ['movie 1', 'movie 2']
+    });
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['movie 1', 'movie 2']
+    }));
+  });
+
+});

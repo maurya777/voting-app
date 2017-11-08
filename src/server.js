19b7ba2aab89ece73e667dc1eb4f08fec46c84df
Satish Maurya <maurya777@gmail.com>
2017-11-08 22:19:29 +0530

    Broadcast state changes from Redux store

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(() => io.emit('state', store.getState().toJs()));
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJs());
+  });
 }
